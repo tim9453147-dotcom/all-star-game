d479de8996e5746e960d610398ffcf4c442be666
Tim <tim861125@gmail.com>
2026-07-30 19:04:10 +0800

    config: update Cloudflare Pages & D1 wrangler settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  nitro: {
+    preset: 'cloudflare-pages'
+  },
+
   devtools: { enabled: true },
   modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
 
